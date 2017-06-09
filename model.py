e4f101c8b3550d393fdfc28eeae01bb7e1695400
euler16 <nilayshrivastava1729@gmail.com>
2017-06-09 18:49:54 +0530

    built core algorithm

--- model.py
+++ model.py
@@ -11,9 +11,13 @@ class CharRNN(torch.nn.Module):
 		self.x_size      = x_size
 		self.T           = T
 		self.num_layers  = 1
-
 		self.lstm  = torch.nn.LSTMCell(input_size = self.x_size, hidden_size = hidden_size, bias = True)
-	
+		self.linear = torch.nn.Linear(hidden_size, x_size)
+		self.init_weights()
+
+	def init_weights(self):
+		self.linear.bias.data.fill_(0)
+		self.linear.weight.data.uniform_(-0.1,0.1)
 
 	def forward(self,input, h_0 = None, c_0 = None):
 
@@ -28,10 +32,28 @@ class CharRNN(torch.nn.Module):
 		y_t = []
 		x_t = None
 
+
 		for timestep in len(input):
 
 			x_t = input[timestep].view(1,len(1,x_size))
 			h_t, c_t = self.lstm(x_t, (h_t,c_t))
-			y_t.append(torch.nn.functional.softmax(h_t))
+			y = torch.nn.functional.softmax(self.linear(h_t))
+			y_t.append(y)
 
 		return y_t, h_t, c_t 
+
+	def sample(self, seed_x, h_0, c_0):
+
+		ixes = []
+		x_t = seed_x
+		h_t, c_t = h_0, c_0
+		for timestep in range(self.T):
+
+			h_t, c_t = self.lstm(x_t,(h_t, c_t))
+			p_t = torch.nn.functional.softmax(self.linear(h_t))
+			ix = np.random.choice(range(x_size), p = p_t.ravel())
+			x_t = torch.nn.Variable(torch.zeros(x_t.size()))
+			x_t[0,ix] = 1
+			ixes.append()
+
+		return ixes 
--- train.py
+++ train.py
@@ -20,7 +20,53 @@ seq_length = 25 # number of steps to unroll the RNN for
 learning_rate = 1e-1
 
 charnet   = CharRNN(hidden_size,x_size, seq_length)
-loss_fn   = torch.nn.CrossEntropyLoss()
+loss_fn   = torch.NLLLoss()
 optimizer = torch.optim.Adam(charnet.parameters(), lr = learning_rate)
+
+loss_data = []
+
+for i in  range(1):
+
+	hprev = torch.autograd.Variable(torch.zeros(1,hidden_size), requires_grad = False)
+	cprev = torch.autograd.Variable(torch.zeros(1,hidden_size), requires_grad = False)
+	window_idx = 0
+	iteration = 1
+
+	input_tensor = torch.autograd.Variable(torch.zeros(seq_length,x_size),requires_grad = False)
+	# output_tensor = torch.autograd.Variable(torch.zeros(seq_length,x_size),requires_grad = False)
+	net_tensor = torch.autograd.Variable(torch.zeros(output_tensor.size()),requires_grad = False)
+
+	while window_idx + seq_length + 1 >= len(data):
+
+
+		inputs  = [char_to_ix[ch] for ch in data[window_idx:window_idx+seq_length]]
+		targets = [char_to_ix[ch] for ch in data[window_idx+1:window_idx+seq_length+1]]
+
+	
+		for charac in range(seq_length):
+			input_tensor[charac,inputs[charac]] = 1
+			# output_tensor[charac,targets[charac]] = 1
+
+		if iteration%100 == 0:
+			sample_ix = charnet.sample(input_tensor[0].view(1,x_size), hprev, cprev)
+			txt = ''.join(ix_to_char[ix] for ix in sample_ix)
+			print '----\n %s \n----' % (txt, )
+
+		y_t, hprev, cprev = charnet(input_tensor,hprev,cprev)
+		torch.cat(y_t,net_tensor)
+
+		loss = loss_fn(net_tensor, torch.LongTensor(targets))
+		optimizer.zero_grad()
+		loss.backward()
+		optimizer.step()
+
+		loss_data
+
+		input_tensor.data.fill_(0)
+		output_tensor.data.fill_(0)
+
+		window_idx += seq_length
+		iteration += 1
+
 
 
